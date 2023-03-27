3d77d49340d37f4b4666945edf17e5c96d91dcb9
Illia-tsar <reklamamicloy@gmail.com>
2023-03-27 03:43:22 +0300

    added apply_visualizer method to SimpleVisualizer class

--- src/visual_utils.py
+++ src/visual_utils.py
@@ -1,5 +1,6 @@
 from dataset import register_datasets
 from model_utils.config import gen_cfg
+from detectron2.utils.visualizer import Visualizer, ColorMode
 
 
 class SimpleVisualizer:
@@ -13,3 +14,16 @@ class SimpleVisualizer:
         self.cfg.MODEL.WEIGHTS = self.cfg.MODEL.WEIGHTS if not weights_path else weights_path
 
         register_datasets(data_path, val_size=val_size)
+
+    def _apply_visualizer(self, img, instances, metadata, is_gt=False):
+        visualizer = Visualizer(
+            img,
+            metadata=metadata,
+            scale=1.0,
+            instance_mode=ColorMode.IMAGE
+        )
+        if is_gt:
+            visualizer = visualizer.draw_dataset_dict(instances)
+        else:
+            visualizer = visualizer.draw_instance_predictions(instances)
+        return visualizer.get_image()
